422c0f68483023ed4c2a5098b41c1f03d101ef36
戴兜 <daidou@daidou.pw>
2018-05-29 21:25:31 +0800

    提交

--- apps/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysql_server_name=$_ENV['MYSQL_HOST'];
-$mysql_username=$_ENV['MYSQL_USERNAME'];
-$mysql_password=$_ENV['MYSQL_PASSWORD'];
-$mysql_database=$_ENV['MYSQL_DBNAME'];
-$conn=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database);
-?>
