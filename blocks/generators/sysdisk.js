8c6bfb38ff585fdba3702b6850a89289f512652e
戴兜 <daidou@daidou.pw>
2018-08-21 21:42:35 +0800

    增加「读配置项」「写配置项」拼图

--- blocks/generators/sysdisk.js
+++ blocks/generators/sysdisk.js
@@ -18,7 +18,23 @@ Blockly.JavaScript['sysdisk_writefile'] = function(block) {
 
 Blockly.JavaScript['sysdisk_readfile'] = function(block) {
   var value_filepath = Blockly.JavaScript.valueToCode(block, 'filepath', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
   var code = "readFile(" + value_filepath + ")";
   return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['sysdisk_setkeytext'] = function(block) {
+  var value_inipath = Blockly.JavaScript.valueToCode(block, 'inipath', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inisection = Blockly.JavaScript.valueToCode(block, 'inisection', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inikey = Blockly.JavaScript.valueToCode(block, 'inikey', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inicontent = Blockly.JavaScript.valueToCode(block, 'inicontent', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "setKeyText(" + value_inipath + "," + value_inisection + "," + value_inikey + "," + value_inicontent + ");\n";
+  return code;
+};
+
+Blockly.JavaScript['sysdisk_getkeytext'] = function(block) {
+  var value_inipath = Blockly.JavaScript.valueToCode(block, 'inipath', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inisection = Blockly.JavaScript.valueToCode(block, 'inisection', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_inikey = Blockly.JavaScript.valueToCode(block, 'inikey', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "getKeyText(" + value_inipath + "," + value_inisection + "," + value_inikey + ");\n";
+  return code;
 };
--- blocks/sysdisk.js
+++ blocks/sysdisk.js
@@ -6,59 +6,130 @@ goog.provide('Blockly.Constants.sysdisk');
 goog.require('Blockly.Blocks');
 
 Blockly.defineBlocksWithJsonArray([
-{
-  "type": "sysdisk_getlocalpath",
-  "message0": "获取酷Q运行目录",
-  "output": null,
-  "colour": "%{BKY_SYSDISK_HUE}",
-  "tooltip": "输出酷Q目录，末尾不带 /",
-  "helpUrl": ""
-}
-,
-{
-  "type": "sysdisk_writefile",
-  "message0": "写文件 %1 完整路径 %2 文件内容 %3",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "filepath",
-      "check": "String",
-      "align": "RIGHT"
-    },
-    {
-      "type": "input_value",
-      "name": "filecontent",
-      "check": "String",
-      "align": "RIGHT"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": "%{BKY_SYSDISK_HUE}",
-  "tooltip": "将文本写入本地文件",
-  "helpUrl": ""
-}
-,
-{
-  "type": "sysdisk_readfile",
-  "message0": "读文件 %1 完整路径 %2",
-  "args0": [
-    {
-      "type": "input_dummy"
-    },
-    {
-      "type": "input_value",
-      "name": "filepath",
-      "check": "String",
-      "align": "RIGHT"
-    }
-  ],
-  "output": null,
-  "colour": "%{BKY_SYSDISK_HUE}",
-  "tooltip": "从本地读取文件内容",
-  "helpUrl": ""
-}
+  {
+    "type": "sysdisk_getlocalpath",
+    "message0": "获取酷Q运行目录",
+    "output": null,
+    "colour": "%{BKY_SYSDISK_HUE}",
+    "tooltip": "输出酷Q目录，末尾不带 /",
+    "helpUrl": ""
+  }
+  ,
+  {
+    "type": "sysdisk_writefile",
+    "message0": "写文件 %1 完整路径 %2 文件内容 %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "filepath",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "filecontent",
+        "check": "String",
+        "align": "RIGHT"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_SYSDISK_HUE}",
+    "tooltip": "将文本写入本地文件",
+    "helpUrl": ""
+  }
+  ,
+  {
+    "type": "sysdisk_readfile",
+    "message0": "读文件 %1 完整路径 %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "filepath",
+        "check": "String",
+        "align": "RIGHT"
+      }
+    ],
+    "output": null,
+    "colour": "%{BKY_SYSDISK_HUE}",
+    "tooltip": "从本地读取文件内容",
+    "helpUrl": ""
+  }
+  ,
+  {
+    "type": "sysdisk_setkeytext",
+    "message0": "写配置 %1 完整路径 %2 配置节 %3 配置键(项) %4 配置内容 %5",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "inipath",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "inisection",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "inikey",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "inicontent",
+        "check": "String",
+        "align": "RIGHT"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_SYSDISK_HUE}",
+    "tooltip": "写入一个配置到指定ini文件",
+    "helpUrl": ""
+  }
+  ,
+  {
+    "type": "sysdisk_getkeytext",
+    "message0": "读配置 %1 完整路径 %2 配置节 %3 配置键(项) %4",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "inipath",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "inisection",
+        "check": "String",
+        "align": "RIGHT"
+      },
+      {
+        "type": "input_value",
+        "name": "inikey",
+        "check": "String",
+        "align": "RIGHT"
+      }
+    ],
+    "output": null,
+    "colour": "%{BKY_SYSDISK_HUE}",
+    "tooltip": "从指定ini文件读入配置",
+    "helpUrl": ""
+  }
 ]);
