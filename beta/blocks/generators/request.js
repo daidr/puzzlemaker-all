dcee6558a12c680894ce125bb9529b864346a627
戴兜 <daidr@daidr.me>
2020-04-18 16:38:03 +0800

    beta分支：更新所有HTTP操作拼图

--- beta/blocks/generators/request.js
+++ beta/blocks/generators/request.js
@@ -9,7 +9,7 @@ function randomString(len) {
     var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
     var maxPos = $chars.length;
     var pwd = '';
-    for (i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) {
         pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
 
     }
